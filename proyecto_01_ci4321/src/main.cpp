8ee97ec02641babe6654f8b53379a020499ea57d
jjca <14-10254@usb.ve>
2024-10-27 17:33:10 -0400

    Codigo para camara

--- proyecto_01_ci4321/src/main.cpp
+++ proyecto_01_ci4321/src/main.cpp
@@ -19,6 +19,88 @@ using namespace std;
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
+// Dónde está la cámara
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 5.0f);
+// el fente
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+// vector del eje derech
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;
+float pitch = 0.0f;
+int lastX = WIDTH / 2;
+int lastY = HEIGHT / 2;
+float fov = 45.0f;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// Metodos
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+
+	const float cameraSpeed = 2.5f * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		cameraPos += cameraSpeed * cameraFront;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		cameraPos -= cameraSpeed * cameraFront;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	}
+
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+
+
+	if (firstMouse) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f) {
+		pitch = 89.0f;
+	}
+	if (pitch < -89.0f) {
+		pitch = -89.0f;
+	}
+
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(direction);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+	fov -= (float)yoffset;
+	if (fov < 1.0f)
+		fov = 1.0f;
+	if (fov > 45.0f)
+		fov = 45.0f;
+}
+
 int main(void) {
 
 	GLFWwindow* window;
@@ -39,6 +121,9 @@ int main(void) {
 	/* Indicacion de ventana/lienzo actual para glfw */
 	glfwMakeContextCurrent(window);
 
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	/* Iniciacion de libreria de glew */
 	GLenum glew_status = glewInit();
 	if (glew_status != GLEW_OK) {
@@ -91,12 +176,21 @@ int main(void) {
 	ourShader.use(); 
 	ourShader.setInt("texture1", 0);
 
-	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+	glm::mat4 projection;
 	ourShader.setMat4("projection", projection);
 
 	/* Ciclo hasta que el usuario cierre la ventana */
 	while (!glfwWindowShouldClose(window))
 	{
+		// Clock
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// input
+		processInput(window);
+
+
 		/* Limpieza del buffer y el buffer de profundidad */
 		glClearColor(0.761f, 1.0f, 0.992f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -108,10 +202,18 @@ int main(void) {
 
 		// Aplicamos la matriz del view (hacia donde esta viendo la camara)
 		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -10.0f));
+		view = glm::lookAt(
+			cameraPos,
+			cameraPos + cameraFront,
+			cameraUp
+		);
+		ourShader.setMat4("view", view);
+		/*view = glm::translate(view, glm::vec3(0.0f, 0.0f, -10.0f));
 		unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);*/
 
+		projection = glm::perspective(glm::radians(fov), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+		ourShader.setMat4("projection", projection);
 		tank.Draw(ourShader);
 		glBindVertexArray(0);
 
